8872507eb958477dd8d0e988923a1f40628fb0e3
ricodroid <ricodroid2024@gmail.com>
2025-01-03 08:11:54 +0900

    Update .gitignore

---
+++ MyGoal2025/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyGoal2025
+//
+//  Created by r_murata on 2025/01/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyGoal2025/MyGoal2025App.swift
@@ -0,0 +1,17 @@
+//
+//  MyGoal2025App.swift
+//  MyGoal2025
+//
+//  Created by r_murata on 2025/01/03.
+//
+
+import SwiftUI
+
+@main
+struct MyGoal2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
